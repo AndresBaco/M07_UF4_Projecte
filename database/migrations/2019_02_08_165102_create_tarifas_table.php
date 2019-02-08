ba91daba2a30d21d97644574bf6e48762c785752
AndresBaco <43445646+AndresBaco@users.noreply.github.com>
2019-02-08 18:26:53 +0100

    Andres Baco: Migrations tabla tarifas

---
+++ database/migrations/2019_02_08_165102_create_tarifas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTarifasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarifas', function (Blueprint $table) {
+            $table->increments('tid');
+            $table->string('tipus');
+            $table->integer('preu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarifas');
+    }
+}
---
+++ database/migrations/2019_02_08_170129_add_tid_to_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTidToMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->integer('tid')->unsigned();;
+            $table->foreign('tid')->references('tid')->on('tarifas');
+        });
+       
+
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+
+    }
+}
