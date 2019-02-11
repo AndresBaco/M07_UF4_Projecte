54f56fddfe3e72ecec331b5b2c2140aaf0108a66
AndresBaco <43445646+AndresBaco@users.noreply.github.com>
2019-02-11 19:51:14 +0100

    Andres Baco: TarifasController y añadir rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/login', function () {
 });
 
 Route::group(['middleware' => 'auth'], function(){
+    //movies
     Route::get('/catalog', 'CatalogController@getIndex');
     Route::get('/catalog/show/{id}', 'CatalogController@getShow');
 
@@ -30,6 +31,18 @@ Route::group(['middleware' => 'auth'], function(){
     Route::put('/catalog/rent/{id}', 'CatalogController@putRent');
     Route::put('/catalog/return/{id}', 'CatalogController@putReturn');
     Route::delete('/catalog/delete/{id}', 'CatalogController@deleteMovie');
+
+    //tarifas
+    Route::get('/tarifas', 'TarifasController@getIndex');
+    Route::get('/catalog/tarifas/{id}', 'TarifasController@getShow');
+
+    Route::get('/tarifas/create', 'TarifasController@getCreate');
+    Route::post('/tarifas/create', 'TarifasController@postCreate');
+
+    Route::get('/tarifas/edit/{id}', 'TarifasController@getEdit');
+    Route::put('/tarifas/edit/{id}', 'TarifasController@putEdit');
+
+    Route::delete('/tarifas/delete/{id}', 'TarifasController@deleteTarifa');
 });
 Auth::routes();
 
